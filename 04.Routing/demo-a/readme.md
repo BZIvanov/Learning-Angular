c0d375a99c5b9b2446e935477b84d5f1267e8732
BZIvanov <biser.z.ivanov@gmail.com>
2020-08-15 09:27:48 +0300

    football teams, routing demo project

---
+++ 04.Routing/demo-a/readme.md
@@ -0,0 +1,7 @@
+## Football teams demo
+
+### Angular Material
+
+- This small demo project is build with Angular Material, so some of the content is auto generated, for example the styles in angular.json
+
+To learn more about Material click [here](https://material.angular.io/)
---
+++ 04.Routing/demo-a/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSortModule } from '@angular/material/sort';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DetailsComponent } from './details/details.component';
+import { ListComponent } from './list/list.component';
+import { MapsComponent } from './maps/maps.component';
+
+@NgModule({
+  declarations: [AppComponent, DetailsComponent, ListComponent, MapsComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatSortModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
